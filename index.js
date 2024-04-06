4d8c43a124fa8dd8b1eb59b17caf84f8178059d3
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-04-06 15:57:45 +0530

    create server using node js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
 // global modules and core modules in node js 
-console.log("global module because  you don't want to install it again");
+// console.log("global module because  you don't want to install it again");
 
 
 
 
 // core modules like http,fs,os 
+// const files= require("fs");
+// files.writeFileSync("Sakshi.txt","hello another example of core modules")
 
 
-const files= require("fs");
 
-files.writeFileSync("Sakshi.txt","hello another example of core modules")
+// create server 
+const http =require("http");
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'application\json'});
+    // res.write("Hello Sakshi Maheshwari");
+    res.write(JSON.stringify({name :"sakshi",age :24}));
+    res.end()
+}).listen(8080);

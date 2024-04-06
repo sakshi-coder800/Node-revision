2f6905b7a266103c35dbbcb5f4fb1d07c0151b26
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-04-06 15:50:13 +0530

    core and global modules in node

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// global modules and core modules in node js 
+console.log("global module because  you don't want to install it again");
+
+
+
+
+// core modules like http,fs,os 
+
+
+const files= require("fs");
+
+files.writeFileSync("Sakshi.txt","hello another example of core modules")
